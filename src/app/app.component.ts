2432385f81cd0254187d939cfcaad622915676e9
JorisWijgert <jvdwijgert@hotmail.com>
2025-01-20 22:36:29 +0100

    feat(form): Create form to schedule car maintenances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CarService } from './services/car.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MaintenanceJob } from './models/maintenance-job';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'CarMaintenance';
+export class AppComponent implements OnInit {
+  public title = 'CarMaintenance';
+  public maintenanceJobs: MaintenanceJob[] = [];
+  public scheduleMaintenanceJobForm: FormGroup;
+
+  constructor(public carService: CarService) {
+    this.scheduleMaintenanceJobForm = new FormGroup({
+      maintenanceJob: new FormControl(null),
+    });
+  }
+
+  public ngOnInit(): void {
+    this.carService.maintenanceJobs.subscribe((jobs) => {
+      this.maintenanceJobs = jobs;
+    });
+  }
 }
