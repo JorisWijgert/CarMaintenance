ab822dcfff56d2e87f4003c5221b6e0bccd391bd
JorisWijgert <jvdwijgert@hotmail.com>
2025-01-20 18:18:42 +0100

    feat(upgrade): Update to Angular 17

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SetupScheduledMaintenanceJobComponent } from './setup-scheduled-maintenance-job/setup-scheduled-maintenance-job.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '**', component: SetupScheduledMaintenanceJobComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
