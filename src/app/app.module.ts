227568046d065d12f937976381d3eb682a46d133
JorisWijgert <jvdwijgert@hotmail.com>
2025-01-20 18:00:46 +0100

    feat(components): Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SetupScheduledMaintenanceJobComponent } from './setup-scheduled-maintenance-job/setup-scheduled-maintenance-job.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetupScheduledMaintenanceJobComponent
   ],
   imports: [
     BrowserModule,
