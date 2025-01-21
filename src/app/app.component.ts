ccf83015b602a7a8b86d9e7f31a5179eec8190b6
JorisWijgert <jvdwijgert@hotmail.com>
2025-01-21 14:40:54 +0100

    feat(maintenance-job): Finish up calculation of cost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CarService } from './services/car.service';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MaintenanceJob } from './models/maintenance-job';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
@@ -8,6 +13,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTimepickerModule } from '@angular/material/timepicker';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +25,10 @@ import { provideNativeDateAdapter } from '@angular/material/core';
     MatSelectModule,
     MatInputModule,
     MatTimepickerModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatButtonModule,
+    FormsModule,
+    DecimalPipe
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -30,6 +40,14 @@ export class AppComponent implements OnInit {
   public scheduleMaintenanceJobForm: FormGroup;
   public dateTimeForm: FormGroup;
   public selectedMaintenanceJob?: MaintenanceJob;
+  public selectedDate: Date = new Date();
+  public markupWeekend = 100;
+  public costOfScheduledMaintenanceJobWithoutTax = 0;
+  public costOfScheduledMaintenanceJob = 0;
+  public readonly taxPercentage = 21;
+  public readonly hourRate = 60;
+
+  private readonly markupIfWeekend = 150;
 
   constructor(public carService: CarService) {
     this.scheduleMaintenanceJobForm = new FormGroup({
@@ -37,9 +55,9 @@ export class AppComponent implements OnInit {
     });
 
     this.dateTimeForm = new FormGroup({
-      date: new FormControl(null),
-      time: new FormControl(null),
-    })
+      date: new FormControl(Date.now()),
+      time: new FormControl(Date.now()),
+    });
   }
 
   public ngOnInit(): void {
@@ -53,5 +71,33 @@ export class AppComponent implements OnInit {
     this.selectedMaintenanceJob = this.maintenanceJobs.find(
       (job) => job.id === formValue
     );
+  }
+
+  public calculate() {
+    if (!this.selectedMaintenanceJob) {
+      return;
+    }
+
+    this.markupWeekend = 100;
+    if(this.selectedDate.getDay() === 6 || this.selectedDate.getDay() === 0) {
+      this.markupWeekend = this.markupIfWeekend;
+    }
+    let sum = 0;
+
+    if (this.selectedMaintenanceJob.spareParts && this.selectedMaintenanceJob.spareParts.length > 0) {
+      this.selectedMaintenanceJob.spareParts.forEach((part) => {
+        sum += part.cost;
+      })
+    }
+
+    sum += this.selectedMaintenanceJob?.serviceHours * this.hourRate;
+
+    sum = (sum/100) * this.markupWeekend;
+
+    this.costOfScheduledMaintenanceJobWithoutTax = sum;
+
+    sum = (sum/100) * (this.taxPercentage + 100);
+
+    this.costOfScheduledMaintenanceJob = sum;
   }
 }
